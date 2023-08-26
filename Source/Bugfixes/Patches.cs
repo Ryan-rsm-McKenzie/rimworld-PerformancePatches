6a3e3116b9b4e1d379c424eb0d1de162a34f7d59
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2023-08-26 01:44:30 -0700

    add a bugfix for the bestowing ceremony

---
+++ Source/Bugfixes/Patches.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable IDE1006 // Naming Styles
+
+using System;
+using HarmonyLib;
+using RimWorld;
+
+namespace PerformancePatches.Precepts
+{
+	[HarmonyPatch(typeof(QuestPart_Bestowing_TargetChangedTitle))]
+	[HarmonyPatch("Notify_QuestSignalReceived")]
+	[HarmonyPatch(new Type[] { typeof(Signal) })]
+	internal class QuestPart_Bestowing_TargetChangedTitle_Notify_QuestSignalReceived
+	{
+		public static bool Prefix(QuestPart_Bestowing_TargetChangedTitle __instance)
+		{
+			if ((__instance.pawn?.Dead ?? true) || (__instance.bestower?.Dead ?? true)) {
+				__instance.quest?.End(QuestEndOutcome.Unknown, false);
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+}
