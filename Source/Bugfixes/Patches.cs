81e96de13ad716beceec3bbf1feb55e298e51b22
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2023-09-28 00:52:38 -0700

    bugfixes for tynan's stupidity

--- Source/Bugfixes/Patches.cs
+++ Source/Bugfixes/Patches.cs
@@ -4,8 +4,9 @@
 using System;
 using HarmonyLib;
 using RimWorld;
+using Verse;
 
-namespace PerformancePatches.Precepts
+namespace PerformancePatches.Bugfixes
 {
 	[HarmonyPatch(typeof(QuestPart_Bestowing_TargetChangedTitle))]
 	[HarmonyPatch("Notify_QuestSignalReceived")]
@@ -16,6 +17,38 @@ namespace PerformancePatches.Precepts
 		{
 			if ((__instance.pawn?.Dead ?? true) || (__instance.bestower?.Dead ?? true)) {
 				__instance.quest?.End(QuestEndOutcome.Unknown, false);
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(RestUtility))]
+	[HarmonyPatch("IsCharging")]
+	[HarmonyPatch(new Type[] { typeof(Pawn) })]
+	internal class RestUtility_IsCharging
+	{
+		public static bool Prefix(ref bool __result, Pawn p)
+		{
+			if (p.needs is null) {
+				__result = false;
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(RestUtility))]
+	[HarmonyPatch("IsSelfShutdown")]
+	[HarmonyPatch(new Type[] { typeof(Pawn) })]
+	internal class RestUtility_IsSelfShutdown
+	{
+		public static bool Prefix(ref bool __result, Pawn p)
+		{
+			if (p.needs is null) {
+				__result = false;
 				return false;
 			} else {
 				return true;
